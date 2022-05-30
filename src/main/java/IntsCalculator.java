7403635d07ecf23b75ef481af2e43495d78b6188
evgeny <evgeniyvaganov@yandex.ru>
2022-05-30 21:28:33 +0500

    commit

--- src/main/java/IntsCalculator.java
+++ src/main/java/IntsCalculator.java
@@ -1,15 +1,17 @@
 public class IntsCalculator implements Ints {
     protected final Calculator target;
 
-    public IntsCalculator() { this.target = new Calculator(); }
+    public IntsCalculator() {
+        this.target = new Calculator();
+    }
 
     @Override
     public int sum(int arg0, int arg1) {
-       return (int) target.newFormula()
-               .addOperand(arg0)
-               .addOperand(arg1)
-               .calculate(Calculator.Operation.SUM)
-               .result();
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM)
+                .result();
     }
 
     @Override
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Ints calc = new IntsCalculator();
-        System.out.println(calc.sum(2,5));
-        System.out.println(calc.mult(10,15));
-        System.out.println(calc.pow(3,2));
+        System.out.println(calc.sum(2, 5));
+        System.out.println(calc.mult(10, 15));
+        System.out.println(calc.pow(3, 2));
     }
 }
